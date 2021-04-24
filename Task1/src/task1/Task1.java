3afb852ce716f5559e6f2af04612b41885b00a21
AhmadArshad123 <ahmadjutt454979@gmail.com>
2021-04-24 15:13:33 +0500

    Task1(Function 1+2)
    Functions of  wordCounter and vowelCounter

--- Task1/src/task1/Task1.java
+++ Task1/src/task1/Task1.java
@@ -5,6 +5,8 @@
  */
 package task1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author hp
@@ -14,8 +16,56 @@ public class Task1 {
     /**
      * @param args the command line arguments
      */
+    
+    public static int wordCounter(String s){
+        int count=0;
+        int len=s.length();
+        char [] arr= new char[len];
+        arr=s.toCharArray();
+        for(int i=0 ; i<len ; i++)
+        {
+            if(arr[i]==' ')
+            {
+                count++;
+            }
+        }
+        //for the last word count
+        count++;
+        
+        return count;
+        
+    }
+    
+    public static int vowelcounter(String s)
+    {
+        int count = 0;
+        int len=s.length();
+        char [] arr = new char[len];
+        arr=s.toCharArray();
+        for(int i=0 ; i<len ; i++)
+        {
+            if((arr[i]=='A' || arr[i]=='a') || (arr[i]=='I' || arr[i]=='i'))
+            {
+                count++;
+            }
+            if((arr[i]=='E' || arr[i]=='e') || (arr[i]=='O' || arr[i]=='o') || (arr[i]=='U' || arr[i]=='u'))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner s;
+        s= new Scanner(System.in);
+        
+        System.out.println("Enter String");
+        String input;
+        input=s.nextLine();
+        int noofword=wordCounter(input);
+         int noofvowels=vowelcounter(input);
     }
     
 }
