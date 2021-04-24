3eda88a716454c671d35a76fa79a0fc98260c452
Muhammad-Usman-Rasheed <81910088+Muhammad-Usman-Rasheed@users.noreply.github.com>
2021-04-24 15:07:19 +0500

    Task 2 code Vertically display String

---
+++ Task2/src/task2/Task2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task2;
+import java.util.*;
+
+/**
+ *
+ * @author ur919
+ */
+public class Task2 {
+    static void displayStringVerical(String s){
+        System.out.println("Your Sentence Printed Vertically is:");
+        for(int i=0 ;i< s.length();i++)
+        {
+            if(s.charAt(i)!=' ')
+            System.out.println(s.charAt(i));
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner reader= new Scanner (System.in);
+    System.out.println("Please Enter a Sentence:");
+    String input = reader.nextLine();
+    displayStringVerical(input);
+    }
+    
+}
